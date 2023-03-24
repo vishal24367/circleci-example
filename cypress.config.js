4409d7b4930552369b038d0e92a316d517576bc6
Andrew Goldis <agoldis@gmail.com>
2023-03-24 14:46:02 -0700

    use cypress-cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require("cypress");
-
+const { cloudPlugin } = require("@cypress/cloud-plugin");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://en.wikipedia.org/",
     specPattern: "cypress/integration/*.spec.js",
     supportFile: "cypress/support/e2e.js",
+    setupNodeEvents: (on, config) => {
+      return cloudPlugin(on, config);
+    },
   },
-  projectId: "Ij0RfK",
   video: true,
   videoUploadOnPasses: false,
 });
