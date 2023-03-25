c31514ac947ef3d03ea52ab8340d14486ccec96c
Andrew Goldis <agoldis@gmail.com>
2023-03-25 00:04:23 -0700

    use todo

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,8 @@ import { cloudPlugin } from "cypress-cloud/plugin";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "https://en.wikipedia.org/",
-    specPattern: "cypress/integration/*.spec.js",
+    baseUrl: "https://todomvc.com/examples/vanillajs",
+    specPattern: "cypress/e2e/*.spec.js",
     supportFile: "cypress/support/e2e.js",
     setupNodeEvents: (on, config) => {
       return cloudPlugin(on, config);
