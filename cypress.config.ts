c547560aa70d69232126b39f620e25328c6e7c5e
Andrew Goldis <agoldis@gmail.com>
2023-03-25 00:07:57 -0700

    ..

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ import { cloudPlugin } from "cypress-cloud/plugin";
 export default defineConfig({
   e2e: {
     baseUrl: "https://todomvc.com/examples/vanillajs",
-    specPattern: "cypress/e2e/*.spec.ts",
+    specPattern: "cypress/e2e/*.spec.js",
     supportFile: "cypress/support/e2e.js",
     setupNodeEvents: (on, config) => {
       return cloudPlugin(on, config);
